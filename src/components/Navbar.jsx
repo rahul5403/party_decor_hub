04b5a703531cdc908711d2b315640c4d3e64999d
rahul5403 <rahul54meshram@gmail.com>
2025-02-25 16:24:14 +0530

    navbar updated, responsive, bg-g

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,11 +17,16 @@ const NavBar = () => {
   };
 
   const handleLogout = () => {
-    dispatch(logout()); 
+    dispatch(logout());
   };
 
   return (
-    <Navbar bg="light" expand="lg" className="shadow-sm navbar-custom" expanded={expanded}>
+    <Navbar
+      bg="light"
+      expand="lg"
+      className="shadow-sm navbar-custom"
+      expanded={expanded}
+    >
       <Container fluid>
         <Navbar.Toggle
           aria-controls="navbar-nav"
@@ -35,25 +40,50 @@ const NavBar = () => {
 
         <Navbar.Collapse id="navbar-nav">
           <Nav className="mx-auto">
-            <Link to="/home" className="nav-link" onClick={handleNavLinkClick}>Home</Link>
-            <Link to="/party" className="nav-link" onClick={handleNavLinkClick}>Party</Link>
-            <Link to="/decoration" className="nav-link" onClick={handleNavLinkClick}>Decoration</Link>
-            <Link to="/disposable" className="nav-link" onClick={handleNavLinkClick}>Disposable</Link>
-            <Link to="/about" className="nav-link" onClick={handleNavLinkClick}>About Us</Link>
+            <Link to="/home" className="nav-link" onClick={handleNavLinkClick}>
+              Home
+            </Link>
+            <Link to="/party" className="nav-link" onClick={handleNavLinkClick}>
+              Party
+            </Link>
+            <Link
+              to="/decoration"
+              className="nav-link"
+              onClick={handleNavLinkClick}
+            >
+              Decoration
+            </Link>
+            <Link
+              to="/disposable"
+              className="nav-link"
+              onClick={handleNavLinkClick}
+            >
+              Disposable
+            </Link>
+            <Link to="/about" className="nav-link" onClick={handleNavLinkClick}>
+              About Us
+            </Link>
           </Nav>
         </Navbar.Collapse>
-        
-        <div className="rt">
+
+        <div className="d-flex align-items-center">
           <div className="nav-user-icon">
             {isAuthenticated ? (
               <Dropdown>
-                <Dropdown.Toggle id="user-dropdown" className="user-icon-dropdown">
+                <Dropdown.Toggle
+                  id="user-dropdown"
+                  className="user-icon-dropdown"
+                >
                   <FaUserCircle size={30} />
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu className="user-dropdown-menu">
-                  <Dropdown.Item as={Link} to="/profile">My Profile</Dropdown.Item>
-                  <Dropdown.Item as={Link} to="/orders">My Orders</Dropdown.Item>
+                  <Dropdown.Item as={Link} to="/profile">
+                    My Profile
+                  </Dropdown.Item>
+                  <Dropdown.Item as={Link} to="/orders">
+                    My Orders
+                  </Dropdown.Item>
                   <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
@@ -63,14 +93,12 @@ const NavBar = () => {
               </Link>
             )}
           </div>
-          {isAuthenticated && (
-            <div>
-              <Link to="/cart" className="nav-link cart-icon p-1">
-                <FaShoppingCart size={30} />
-                {cartCount > 0 && <span className="cart-badge">{cartCount}</span>}
-              </Link>
-            </div>
-          )}
+          <div>
+            <Link to="/cart" className="nav-link cart-icon p-1">
+              <FaShoppingCart size={30} />
+              {cartCount > 0 && <span className="cart-badge">{cartCount}</span>}
+            </Link>
+          </div>
         </div>
       </Container>
     </Navbar>
